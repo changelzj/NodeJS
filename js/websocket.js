f71176d00e0d90160209676b4d3db5b8a9c9d67b
LiuZijian <lzj@123>
2019-05-29 23:57:26 +0800

    add

---
+++ js/websocket.js
@@ -0,0 +1,20 @@
+var WebSocketServer = require('ws').Server;
+
+var ws = new WebSocketServer({port:9000});
+var clientMap = {};
+
+ws.on('connection', function (ws) {
+    console.log(ws + '上线');
+    clientMap[ws.name] = ws;
+});
+
+ws.on('message', function (message) {
+    for (key in clientMap) {
+        clientMap[key].send(message);
+    }
+});
+
+ws.on('close', function (ws) {
+    global.gc();
+});
+
--- readme.md
+++ readme.md
@@ -3,9 +3,13 @@
 * npm -v ：查看npm版本
 * npm version ：查看所有模块的版本
 * npm search 包名 ： 搜索包
-* npm install ：安装
+* npm install ：安装package.json内的依赖
 * npm remove ：移除
 * npm install 包名 --save ：安装包并添加到依赖中
+* npm install 包名 --save -dev ：安装包并添加到开发依赖中
 * npm install 包名 -g ：全局安装包(一般不是项目需要)
 * npm install ：下载当前项目所依赖的包
+
+* npm init 初始化package.json
+
 
